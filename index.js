44cc44465369f6b3c45a3fc935065cd8e869a87f
Jason Prince <jasoncprince@gmail.com>
2019-09-04 15:54:15 -0600

    index and server configured

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+const server = require("./server.js");
+
+const port = 8002;
+
+server.listen(port, () => {
+  console.log(`\n*** Server running on port ${port} ***\n`);
+});
--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const express = 'express';
-
+const express = require("express");
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+const userRouter = require("./users/userRouter");
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
+server.use(express.json());
+server.use(logger);
+server.use("/user", userRouter);
 
 function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+  next();
+}
 
-};
+server.get("/", (req, res) => {
+  res.send(`testing`);
+});
 
 module.exports = server;
